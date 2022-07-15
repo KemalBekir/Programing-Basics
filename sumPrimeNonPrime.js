bc6cbc325aa084562b5d0bce466ed23a2b1c92b8
KemalBekir <95451696+KemalBekir@users.noreply.github.com>
2022-07-15 23:55:16 +0100

    Programming Basics - Exercises
    Nested Loop - Exercise

---
+++ sumPrimeNonPrime.js
@@ -0,0 +1,58 @@
+function sumPrimeNonPrime(input) {
+    let index = 0;
+    let command = input[index];
+    index++;
+    let sumPrimeNumbers = 0;
+    let sumNonPrimeNumber = 0;
+    while (command !== "stop") {
+        let num = Number(command);
+        if (num < 0) {
+            console.log(`Number is negative.`);
+            command = input[index];
+            index++;
+            continue;
+        }
+        if (num === 1) {
+            sumPrimeNumbers += num;
+            command = input[index];
+            index++;
+            continue;
+        }
+        let isPrime = true;
+        for (let i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i === 0) {
+                isPrime = false;
+                break;
+            }
+        }
+        if (isPrime) {
+            sumPrimeNumbers += num;
+        } else {
+            sumNonPrimeNumber += num;
+        }
+        command = input[index];
+        index++;
+    }
+
+    console.log(`Sum of all prime numbers is: ${sumPrimeNumbers}`);
+    console.log(`Sum of all non prime numbers is: ${sumNonPrimeNumber}`);
+}
+
+sumPrimeNonPrime(["3",
+    "9",
+    "0",
+    "7",
+    "19",
+    "4",
+    "stop"])
+
+sumPrimeNonPrime(["30",
+    "83",
+    "33",
+    "-1",
+    "20",
+    "stop"])
+sumPrimeNonPrime(["0",
+    "-9",
+    "0",
+    "stop"])
