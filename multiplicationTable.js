d6865e7c3f66ade5e7ff3410895e7d5848a83586
KemalBekir <95451696+KemalBekir@users.noreply.github.com>
2022-07-20 16:58:53 +0100

    Programming Basics - Exercises
    Nested Loops - Exercise

--- multiplicationTable.js
+++ multiplicationTable.js
@@ -1,12 +1,9 @@
-function multiplicationTable(input){
-    let n = Number(input[0]);
-    let result = 0;
-
-    for (let i = 1; i <= 10; i++){
-        result = i * n;
-        console.log(`${i} * ${n} = ${result}`);
+function multiplication() {
+    for (let i = 1; i <= 10; i++) {
+        for (let j = 1; j <= 10; j++) {
+            console.log(`${i} * ${j} = ${i * j}`);
+        }
     }
-
 }
 
-multiplicationTable(["3"])
+multiplication()
