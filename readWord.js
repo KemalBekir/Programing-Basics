c398fee16fba912eb93cbd8efde80223a21f7ffc
KemalBekir <95451696+KemalBekir@users.noreply.github.com>
2022-08-01 18:36:32 +0100

    Programming Basics - Exercises
    While Loop - Exercise

---
+++ readWord.js
@@ -0,0 +1,23 @@
+function readWord(input){
+    let index = 0;
+
+    while(true){
+        let str = input[index];
+        index++;
+        if(str === "Stop"){
+            break;
+        }
+        console.log(str); 
+    }
+
+}
+
+readWord(["Nakov",
+"SoftUni",
+"Sofia",
+"Bulgaria",
+"SomeText",
+"Stop",
+"AfterStop",
+"Europe",
+"HelloWorld"])
